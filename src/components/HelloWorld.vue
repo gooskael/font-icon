b22e323085dc9ff4adb58a76af03010754e64972
EANOTSON <2207174497@qq.com>
2021-07-30 11:48:42 +0800

    字体和图标库引入

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="hello">
+  <div>
+    <!-- iconfont -->
+    <!-- <i class="iconfont icon-dianzan"></i> -->
+    <span class="iconfont icon-dianzan size-change"></span>
+    <i class="iconfont icon-icon-test"></i>
+    <!-- <i class="iconfont icon-iconfonticon-baobia"></i> -->
+    <!-- <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-dianzan_huaban1"></use>
+    </svg> -->
+
+    <!-- font-family -->
+    <h1 class="sans-font"> {{ msg }} </h1>
+    <h1 class="sc-sans-font"> {{ msg }} </h1>
+    <h1 class="k-sans-font"> {{ msg }} </h1>
     <h1>{{ msg }}</h1>
+  
+    <p class="sans-font">
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+    </p>
+    <p class="sc-sans-font">
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+    </p>
+    <p class="k-sans-font">
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+    </p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p class="sans-font">
+      这里尝试打一些中文的字体 完美氪电子科技
+    </p>
+    <p class="sc-sans-font">
+      这里尝试打一些中文的字体 完美氪电子科技
+    </p>
+    <p class="k-sans-font">
+      这里尝试打一些中文的字体 完美氪电子科技
+    </p>
+    <p class="source-sans-font">
+      这里尝试打一些中文的字体 完美氪电子科技&Theta;
+    </p>
+    <p>
+      这里尝试打一些中文的字体 完美氪电子科技&frac34;
+    </p>
   </div>
 </template>
 
@@ -41,6 +60,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.size-change {
+  font-size: 40px;
+  color: orange;
+}
 h3 {
   margin: 40px 0 0;
 }
